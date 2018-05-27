77b17640b9cbcc243ee62cf0f5b6b04b71547232
cassiogvstos <cassiogvstos@gmail.com>
2018-05-27 16:53:10 -0300

    Aterado a pasta do projeto

--- server.js
+++ server.js
@@ -23,6 +23,22 @@ var router = express.Router();//intercepta todas as rotas
 
 //MIDDLEWARE pega antes de chegar na rota desejada
 router.use(function(req,res,next){
+	/*OBSERVAÇÃO - Para que a API tenha autorização de acessar ou ser acessada por outro host precisa-se 
+		redefinir o Header*/
+	// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
 	console.log("Interceptação pelo Middleware OK");
 	next();
 });
