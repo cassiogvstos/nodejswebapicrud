856bca950792e5ca20985dda492624927be184b8
cassiogvstos <cassiogvstos@gmail.com>
2018-06-09 15:50:32 -0300

    agora vai 2  mlab

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var Usuario = require('./app/models/user');
 
 /*Persistência Conexao com o banco (Cloud - MLAB)*/
 try {
-	mongoose.connect('mongodb://acessaporra:123foicarai@ds016718.mlab.com:16718/nodeapi')
+	mongoose.connect('mongodb://<acessaporra>:<123foicarai>@ds016718.mlab.com:16718/nodeapi')
 } catch (e) {}
 
 
